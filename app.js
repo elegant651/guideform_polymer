36641277929674fe58a397e6fbe69fe9fffd60f6
elegantuniv <korea651@gmail.com>
2015-02-20 15:59:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var temp_app = document.querySelector('#temp_app');
+
+temp_app.groups = [		    	
+	{title: "Getting Started", img: "dtest.png", categories: [{title: "KnowRe Learning"}, {title:"Highlight Features", sub: ["Navigation", "Coins & Stars", "Base Camp", "Notifications"]}]},
+	{title: "Notices", img: "dtest.png", categories: [{title: "Announcements"}, {title:"Update Notes"}, {title:"Out Newest Features"}]},
+	{title: "Solve Questions", img: "dtest.png", categories: [{title:"Walk Me Through"}, {title:"Keyboard"}, {title:"Graphing"}, {title:"Number lines"}]},
+	{title: "Manage Your Learning", img: "dtest.png", 
+		categories: [
+		{
+			title:"Achievement System", 
+			content: {
+				intro: "KnowRe's CCSS-aligned curricula provide an enhanced - explanation to page",				
+				pars:[
+					{subtitle: "Classroom curriculum designed to provide personalized support - subtitle", img: "dtest.png", detail: "-KnowRe's complete curricula can be assigned : detail"},
+					{subtitle: "Classroom curriculum designed to provide personalized support - subtitle", img: "dtest.png", detail: "-KnowRe's complete curricula can be assigned : detail"}
+				]		
+			}
+		}, 
+		{title:"Lesson"}, {title:"Assignment"}]}		    	
+];	
